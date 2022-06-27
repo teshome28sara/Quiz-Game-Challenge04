a03b49137625234eade86295b4d6b023e00e906b
sara <tes28sar@gmail.com>
2022-06-26 21:48:27 -0700

    working on displayQuestion function

--- script.js
+++ script.js
@@ -2,18 +2,101 @@ var timerEl = document.getElementById('countdown');
 var count;
 var btnStart = document.getElementById("btn-start");
 var welcome = document.getElementById("welcome");
-var questionPosition = 0;
-var curruntQuestion = questions[questionPosition]
+var questionPosition= 0;
+var divEl = document.querySelector("#container");
+var scores = 0;
 
 function displayeQuestion(){
-  var divEl = document.querySelector("#container");
-  for(var i = 0; i < divEl.length; i++){
-  divEl = curruntQuestion
-  divEl.append(curruntQuestion);
-  }
-  startQuiz();
- 
+
+
+    
+
+ var curruntQuestion = questions[questionPosition] 
+ var divEl = document.querySelector("#container");
+ var body = document.body
+
+ var pEl = document.createElement("p")
+ pEl.textContent = curruntQuestion.name
+ body.appendChild(divEl)
+ divEl.appendChild(pEl)
+ var buttonEl = document.createElement("button");
+ buttonEl.textContent = curruntQuestion.answers[0];
+ body.appendChild(divEl)
+ divEl.appendChild(buttonEl)
+ var buttonEl = document.createElement("button");
+ buttonEl.textContent = curruntQuestion.answers[1];
+ body.appendChild(divEl)
+ divEl.appendChild(buttonEl)
+ var buttonEl = document.createElement("button");
+ buttonEl.textContent = curruntQuestion.correct;
+ body.appendChild(divEl)
+ divEl.appendChild(buttonEl)
 }
+
+function nextQuestion(){
+    questionPosition ++;
+    displayeQuestion();
+}
+
+   
+
+
+
+
+ 
+ 
+  
+
+
+
+
+
+
+
+    
+
+
+
+
+  
+
+
+
+
+ 
+
+ 
+
+ 
+
+
+ 
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+ 
+
 
 
   
@@ -24,12 +107,15 @@ function displayeQuestion(){
 var questions =[
     {
         name: "what is the biggest body of water in the world?",
-         answers:["Atlantic", "Pacific", "Arctic"],
+         answers: ["Atlantic",
+          "Arctic"],
         correct:"Pacific"
     },
     {
         name: "what is the biggest mountin in the world?",
-         answers:["Mount Rainier", "Mount Baker", "Mount Everest"],
+         answers:["Mount Rainier",
+         "Mount Kibo"],
+
         correct:"Mount Everest"
     },
     {
@@ -38,12 +124,14 @@ var questions =[
         correct: "Olympia"
     }
  ];
- function startQuiz( ){
-  btnStart.style.visibility = "hidden";
-  welcome.style.visibility = "hidden";
-  countdown();
+//  function startQuiz( ){
+//   btnStart.style.visibility = "hidden";
+//   welcome.style.visibility = "hidden";
+//   countdown();
 
- }
+//  }
+
+
 
 
 
@@ -71,30 +159,31 @@ var questions =[
     
  
 
-// Timer that counts down from 5
-function countdown() {
-  var timeLeft = 75;
+// Timer that counts down from 75
+// function countdown() {
+//   var timeLeft = 75;
 
-  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-  var timeInterval = setInterval(function () {
-    // As long as the `timeLeft` is greater than 1
-    if (timeLeft > 1) {
-      // Set the `textContent` of `timerEl` to show the remaining seconds
-      timerEl.textContent = timeLeft + ' seconds remaining';
-      // Decrement `timeLeft` by 1
-      timeLeft--;
-    } else if (timeLeft === 1) {
-      // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-      timerEl.textContent = timeLeft + ' second remaining';
-      timeLeft--;
-    } else {
-      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-      timerEl.textContent = '';
-      // Use `clearInterval()` to stop the timer
-      clearInterval(timeInterval);
-      // Call the `displayMessage()` function
-    //    displayQuestion();
-    }
-  }, 1000);
-}
-btnStart.addEventListener('click',displayeQuestion);
+//   // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+//   var timeInterval = setInterval(function () {
+//     // As long as the `timeLeft` is greater than 1
+//     if (timeLeft > 1) {
+//       // Set the `textContent` of `timerEl` to show the remaining seconds
+//       timerEl.textContent = timeLeft + ' seconds remaining';
+//       // Decrement `timeLeft` by 1
+//       timeLeft--;
+//     } else if (timeLeft === 1) {
+//       // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+//       timerEl.textContent = timeLeft + ' second remaining';
+//       timeLeft--;
+//     } else {
+//       // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+//       timerEl.textContent = '';
+//       // Use `clearInterval()` to stop the timer
+//       clearInterval(timeInterval);
+//       // Call the `displayMessage()` function
+//     //    displayQuestion();
+//     }
+//   }, 1000);
+// }
+
+btnStart.addEventListener("click" , nextQuestion);
